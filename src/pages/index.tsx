08a2b760f34c27a955fcce19e61a03887d7e7623
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-03-08 17:20:36 +0700

    feat: add transition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  });
+
   return (
     <>
       <Head>
@@ -10,9 +20,46 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white min-h-screen flex flex-col gap-10 justify-center items-center">
-          <Image src="/Images/logo-primary.png" alt="Agrilink Primary Logo" width={162} height={101}/>
-          <p className="font-fredoka text-[18px]">Better Agriculture In A Digital Way</p>
+      <main
+        className={
+          "bg-white min-h-screen flex flex-col gap-10 justify-center items-center transition duration-[600ms] " +
+          (loading ? "" : "bg-green-100")
+        }
+      >
+        {/* //? Pre-loading */}
+        <div
+          className={
+            "flex flex-col justify-center items-center transition duration-[400ms] " +
+            (loading ? "" : "hidden")
+          }
+        >
+          <Image
+            src="/Images/logo-primary.png"
+            alt="Agrilink Primary Logo"
+            width={162}
+            height={101}
+          />
+          <p className="font-fredoka text-[18px]">
+            Better Agriculture In A Digital Way
+          </p>
+        </div>
+        {/* //? Post-loading */}
+        <div
+          className={
+            "flex flex-col justify-center items-center transition duration-[1000ms] " +
+            (loading ? "hidden" : "")
+          }
+        >
+          <Image
+            src="/Images/logo-secondary.png"
+            alt="Agrilink Logo"
+            width={162}
+            height={101}
+          />
+          <p className="font-fredoka text-[18px] text-white">
+            It's time to start your journey with us
+          </p>
+        </div>
       </main>
     </>
   );
